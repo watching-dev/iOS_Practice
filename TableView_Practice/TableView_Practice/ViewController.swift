708bc269bc4bcc7b0acac9c596cc9c206d787705
watching-dev <coderefactoring90@gmail.com>
2023-09-11 10:11:15 +0900

    [add] Creating TableView Project

---
+++ TableView_Practice/TableView_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TableView_Practice
+//
+//  Created by bro on 2023/09/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
