7a53eb8f6232e0805f2bc6f0a5631d5518fc1225
watching-dev <coderefactoring90@gmail.com>
2023-09-19 23:13:12 +0900

    [feat] 버튼 눌렀을때 랜덤으로 이미지, 텍스트 출력

--- Side_Projects/SymbolRoller/SymbolRoller/SymbolRollerViewController.swift
+++ Side_Projects/SymbolRoller/SymbolRoller/SymbolRollerViewController.swift
@@ -19,13 +19,25 @@ class SymbolRollerViewController: UIViewController {
         
         super.viewDidLoad()
         
-        imageView.image = UIImage(systemName: "moon")
+        // TO-DO:
+        // - symbols에서 랜덤으로 하나 추출
+        // - 이미지와 텍스트 연결
+        
+        reload()
         print("view did load")
     }
     
     @IBAction func buttonTapped(_ sender: Any) {
         
+        reload()
         print("tap")
+    }
+    
+    func reload() {
+        
+        let symbol = symbols.randomElement()!
+        imageView.image = UIImage(systemName: symbol)
+        label.text = symbol
     }
     
 }
