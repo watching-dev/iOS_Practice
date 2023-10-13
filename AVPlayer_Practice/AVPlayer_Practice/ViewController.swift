8da9536e343176aa5243f7c94314ac938730e278
watching-dev <coderefactoring90@gmail.com>
2023-10-13 11:24:42 +0900

    [add] AVPlayer_Practice 프로젝트 생성

---
+++ AVPlayer_Practice/AVPlayer_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AVPlayer_Practice
+//
+//  Created by bro on 2023/10/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
